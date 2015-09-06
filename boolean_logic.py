6a21b2d91c37f70a6e8658a0780c87dd7d98fbd1
Luis Araujo <luis_araujocaballlero@hotmail.com>
2015-09-06 15:32:25 -0600

    Bolean Logic

---
+++ boolean_logic.py
@@ -0,0 +1,23 @@
+#Bolean Logic Python
+a = True
+b = False
+c = True
+d = False
+
+print(a)
+print(b)
+
+print("===")
+
+print(not a)
+print(a and b)
+print(a or b)
+
+print("===")
+print( (a and b) or (c and (not d)))
+
+a = 7>3
+print (a)
+
+c = "Hello" == "Hello"
+print(c=3)
